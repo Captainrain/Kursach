670b3919bbb2f04e70d25bdd3e7e7b9f237e64e3
Daria-Look <105943267+Daria-Look@users.noreply.github.com>
2022-11-05 19:29:32 +0300

    Update main.py

--- kursach/main.py
+++ kursach/main.py
@@ -4,28 +4,27 @@ from pygame.sprite import Group
 
 from Settings import Settings
 from Ship import Ship
-from bullet import *
 from game_functions import game_functions as gf
+from GameStats import GameStats
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+     
     pygame.display.set_caption("KURSACH")   
+    stats = GameStats(ai_settings) #Создание экземпляра для хранения игровой статистики.
     ship = Ship(ai_settings, screen) #Make spaceship   
-    bullets = Group() #Создание группы для хранения пуль.
+    bullets = Group() #Создание группы для хранения пуль.    
+    aliens = Group() #Создание группы пришельцев.       
+    gf.create_fleet(ai_settings, screen, ship, aliens) #Создание флота пришельцев.
        
     while True: #Запуск основного цикла игры.
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        bullets.update()
-
-        #Удаление пуль, вышедших за край экрана.
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-            print(len(bullets))
-
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
